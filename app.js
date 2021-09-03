b30be6d0749c947687ae375f8109045a21b006e5
Choudhary Abdullah <52224233+twoabd@users.noreply.github.com>
2021-09-03 17:35:37 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,81 @@
+'use strict';
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+
+
+// Send a POST request through Postman with input type 'File' and key 'imageComp'
+http.createServer(function (req, res) {
+  if (req.url == '/api') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.imageComp.path;
+      var newpath = '/var/www/sixsilicon.com/' + files.imageComp.name;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File Received');
+        if (files.imageComp.name.split('.').pop() === 'JPG' || files.imageComp.name.split('.').pop() === 'JPEG' || files.imageComp.name.split('.').pop() === 'jpg' || files.imageComp.name.split('.').pop() === 'jpeg') {
+            
+            // File is JPEG
+            res.write('JPG File');
+
+            // Compressing it
+            const { spawn } = require( 'child_process' );
+            const comImage = spawn( 'jpegoptim', ['-m85', '--strip-all', '/var/www/sixsilicon.com/' + files.imageComp.name]);
+
+            // Echoing log
+            comImage.stdout.on( 'data', ( data ) => {
+            console.log( `stdout: ${ data }` );
+            });
+
+        }
+        else  if (files.imageComp.name.split('.').pop() === 'PNG' || files.imageComp.name.split('.').pop() === 'png') {
+            
+            // File is PNG
+            res.write('PNG File');
+
+            // Compressing it
+            const { spawn } = require( 'child_process' );
+            const comImage = spawn( 'jpegoptim', ['-m85', '--strip-all', '/var/www/sixsilicon.com/' + files.imageComp.name]);
+
+            // Echoing log
+            comImage.stdout.on( 'data', ( data ) => {
+            console.log( `stdout: ${ data }` );
+            });
+
+        }
+        else if (files.imageComp.name.split('.').pop() === 'GIF' || files.imageComp.name.split('.').pop() === 'gif') {
+            
+            // File is GIF
+            res.write('GIF File');
+
+            // Compressing it
+            const { spawn } = require( 'child_process' );
+            const comImage = spawn( 'jpegoptim', ['-m85', '--strip-all', '/var/www/sixsilicon.com/' + files.imageComp.name]);
+
+            // Echoing log
+            comImage.stdout.on( 'data', ( data ) => {
+            console.log( `stdout: ${ data }` );
+            });
+            
+        }
+        else if (files.imageComp.name.split('.').pop() === 'WEBP' || files.imageComp.name.split('.').pop() === 'webp') {
+
+            // File is WEBP
+            res.write('WEBP File');
+
+            // Compressing it
+            const { spawn } = require( 'child_process' );
+            const comImage = spawn( 'jpegoptim', ['-m85', '--strip-all', '/var/www/sixsilicon.com/' + files.imageComp.name]);
+
+            // Echoing log
+            comImage.stdout.on( 'data', ( data ) => {
+            console.log( `stdout: ${ data }` );
+            });
+
+        }
+        res.end();
+      });
+ });
+  }
+}).listen(3000);
