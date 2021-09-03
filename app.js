432a6c958f7f33b3049f874376d459807ffc3dfd
Choudhary Abdullah <52224233+twoabd@users.noreply.github.com>
2021-09-03 23:41:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,66 +14,32 @@ http.createServer(function (req, res) {
       var outPath = '/var/www/sixsilicon.com/uploads/_' + files.imageComp.name;
       fs.rename(oldpath, newpath, function (err) {
         if (err) throw err;
+        
         if (files.imageComp.name.split('.').pop() === 'JPG' || files.imageComp.name.split('.').pop() === 'JPEG' || files.imageComp.name.split('.').pop() === 'jpg' || files.imageComp.name.split('.').pop() === 'jpeg') {
-            
-            // File is JPEG
             res.write('JPG File');
-
-            // Compressing it
             const { spawn } = require( 'child_process' );
             const comImage = spawn( 'jpegoptim', ['-m85', '--strip-all', '--overwrite', newpath]);
-
-            // Echoing log
-            comImage.stdout.on( 'data', ( data ) => {
-            console.log( `stdout: ${ data }` );
-            });
-
         }
+        
         else  if (files.imageComp.name.split('.').pop() === 'PNG' || files.imageComp.name.split('.').pop() === 'png') {
-            
-            // File is PNG
             res.write('PNG File');
-
-            // Compressing it
             const { spawn } = require( 'child_process' );
             const comImage = spawn( 'pngquant', ['--force', '--skip-if-larger', '--speed=1', '--strip', '--quality=65-80', newpath]);
 
-            // Echoing log
-            comImage.stdout.on( 'data', ( data ) => {
-            console.log( `stdout: ${ data }` );
-            });
-
         }
-        else if (files.imageComp.name.split('.').pop() === 'GIF' || files.imageComp.name.split('.').pop() === 'gif') {
-            
-            // File is GIF
+        
+        else  if (files.imageComp.name.split('.').pop() === 'GIF' || files.imageComp.name.split('.').pop() === 'gif') {
             res.write('GIF File');
-
-            // Compressing it
             const { spawn } = require( 'child_process' );
             const comImage = spawn( 'gifsicle', ['-O3', newpath]);
-
-            // Echoing log
-            comImage.stdout.on( 'data', ( data ) => {
-            console.log( `stdout: ${ data }` );
-            });
-            
         }
+        
         else if (files.imageComp.name.split('.').pop() === 'SVG' || files.imageComp.name.split('.').pop() === 'svg') {
-
-            // File is SVG
             res.write('SVG File');
-          
-            // Compressing it
             const { spawn } = require( 'child_process' );
             const comImage = spawn( 'scour', ['-i', newpath, '-o', outPath, '--enable-id-stripping', '--enable-comment-stripping', '--shorten-ids', '--indent=none']);
-
-            // Echoing log
-            comImage.stdout.on( 'data', ( data ) => {
-            console.log( `stdout: ${ data }` );
-            });
-
         }
+        
         else {
           res.write('Something Else');
         }
